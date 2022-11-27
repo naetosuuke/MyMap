f58353329c3c05e3df8fff710f5fc936284575ba
naetosuuke <doydoy7e10s@outlook.jp>
2022-11-28 01:36:44 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by Daisuke Doi on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
