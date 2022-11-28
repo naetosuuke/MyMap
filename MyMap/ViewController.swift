cac7a6bf28936afbdbd596480267808e24f2e8d9
naetosuuke <118874380+naetosuuke@users.noreply.github.com>
2022-11-29 07:42:05 +0900

    building

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Text FieldのDelegate通知先を設定
+        inputText.delegate = self
+        
     }
-
-
+  
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //キーボードを閉じる(1)
+        textField.resignFirstResponder()
+        
+        //入力された文字を繰り返す(2)
+        if let searchKey = textField.text{
+            //入力された文字をデバッグエリアに表示(3)
+            print(searchKey)
+            
+        }
+        
+        //デフォルト動作を行うのでTrueを返す(4)
+        return true
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
